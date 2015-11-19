f65c5ece512373f10e7e2a868986c40fe954c21c
Edward Suczewski <ed.suczewski@gmail.com>
2015-11-18 22:43:26 -0700

    cleaning up some stuff
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- Pair/Person.swift
+++ Pair/Person.swift
@@ -20,17 +20,6 @@ struct Person {
         self.name = name
     }
 
-//    func dictionaryCopy() -> Dictionary<Int, String> {
-//        
-//        var dictionary : [Int: String] = [:]
-//        let people = PersonController.sharedController.people
-//        
-//        for n in 0...(people.count - 1) {
-//            dictionary[n] = people[n].name
-//        }
-//        
-//        return dictionary
-//    }
     
     init?(dictionary: Dictionary<String, AnyObject>) {
         guard let name = dictionary[kName] as? String else {
